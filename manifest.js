1e5561edd045a0ca46c4a7dea15fa308d7254843
imhajes <imhajes@gmail.com>
2020-05-11 19:41:34 +0300

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,34 @@
+{
+  "short_name": "Viptools",
+  "name": "Viptools.es",
+  "theme_color": "#1668dc",
+  "background_color": "#FFFFFF",
+  "display": "standalone",
+  "icons": [
+    {
+      "src": "assets/512x512.png",
+      "sizes": "512x512"
+    },
+    {
+      "src": "assets/192x192.png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "assets/144x144.png",
+      "sizes": "144x144"
+    },
+    {
+      "src": "assets/96x96.png",
+      "sizes": "96x96"
+    },
+    {
+      "src": "assets/72x72.png",
+      "sizes": "72x72"
+    },
+    {
+      "src": "assets/48x48.png",
+      "sizes": "48x48"
+    }
+  ],
+  "start_url": "/"
+}
